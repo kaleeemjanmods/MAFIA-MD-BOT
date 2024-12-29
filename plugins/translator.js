124ca508a959e096cb74051afea60ce750e1cf46
MAFIA-MD <adeelsabqi786@gmail.com>
2024-12-29 00:34:54 -0600

    Update translator.js
    Signed-off-by: MAFIA-MD  <adeelsabqi786@gmail.com>

--- plugins/translator.js
+++ plugins/translator.js
@@ -25,7 +25,7 @@ async (conn, mek, m, { from, q, reply }) => {
         const translation = response.data.responseData.translatedText;
 
         const translationMessage = `
-🌍 *PANHWAR MD TRANSLATION* 🌍
+🌍 *MAFIA MD TRANSLATION* 🌍
 
 🔤 *Original*: ${textToTranslate}
 
@@ -33,7 +33,7 @@ async (conn, mek, m, { from, q, reply }) => {
 
 🌐 *Language*: ${targetLang.toUpperCase()}
 
-*PANHWAR-MD CREATION*`;
+*MAFIA-MD CREATION*`;
 
         return reply(translationMessage);
     } catch (e) {
