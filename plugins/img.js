3614f3a9c56ecf2f32d647e7dc37fd431b0ebc16
MAFIA-MD <adeelsabqi786@gmail.com>
2024-12-29 00:15:53 -0600

    Update img.js
    Signed-off-by: MAFIA-MD  <adeelsabqi786@gmail.com>

--- plugins/img.js
+++ plugins/img.js
@@ -43,7 +43,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
  *Enjoy these images! 👾*
 
-> 🌈*PANHWAR MD BOT PLUGINS*🏝️`
+> 🌈*MAFIA MD BOT PLUGINS*🏝️`
 }, { quoted: mek });
 }
 
