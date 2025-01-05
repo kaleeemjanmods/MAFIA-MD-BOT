a423d0cf5d7cb4b0ecec8f4df95ac4a95de79e97
MAFIA-MD <adeelsabqi786@gmail.com>
2025-01-05 09:11:36 +0500

    Update alive.js
    Signed-off-by: MAFIA-MD  <adeelsabqi786@gmail.com>

--- plugins/alive.js
+++ plugins/alive.js
@@ -18,7 +18,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ┃◈┃• *⏳Uptime*:  ${runtime(process.uptime())} 
 ┃◈┃• *📟 Ram usage*: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}MB
 ┃◈┃• *⚙️ HostName*: ${os.hostname()}
-┃◈┃• *👨‍💻 Owner*: ᴀɴsᴀʀ-ᴘᴀɴʜᴡᴀʀ
+┃◈┃• *👨‍💻 Owner*: ☆° ﾟ𝙼𝙰𝙵𝙸𝙰-𝙰𝙳𝙴𝙴𝙻ﾟ°☆
 ┃◈┃• *🧬 Version*: 2.0.0
 ┃◈└───────────┈⊷
 ╰──────────────┈⊷
