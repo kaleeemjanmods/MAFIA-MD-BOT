5dee8260b7dac0be55e801ffa21d1d2de31157fb
MAFIA-MD <adeelsabqi786@gmail.com>
2025-01-12 22:29:52 +0500

    Update alive.js
    Signed-off-by: MAFIA-MD  <adeelsabqi786@gmail.com>

--- plugins/alive.js
+++ plugins/alive.js
@@ -26,7 +26,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
         // Send the status message with an image
         await conn.sendMessage(from, { 
-            image: { url: `https://i.ibb.co/hc1P121/Manul-Ofc-X.jpg` },  // Image URL
+            image: { url: `https://i.ibb.co/Z8JfJpV/Manul-Ofc-X.jpg` },  // Image URL
             caption: status,
             contextInfo: {
                 mentionedJid: [m.sender],
